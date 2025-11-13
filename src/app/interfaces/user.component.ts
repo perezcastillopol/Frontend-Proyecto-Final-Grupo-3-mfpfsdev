479c5a6c97cb287a39803229d2e5b574a7216310
Elmar Taghiyev Mamedova <elmartaguiyev@gmail.com>
2025-11-13 17:14:33 +0100

    Creo una interfaz de usuario momentánea

---
+++ src/app/interfaces/user.component.ts
@@ -0,0 +1,14 @@
+export interface User {
+  id: string;
+  fullName: string;
+  username: string;
+  rating: number;
+
+  bio: string;
+  interests: string[];
+
+  phone: string;
+  birthDate: string;  // formato 'YYYY-MM-DD'
+  location: string;
+  travelStyle: string;
+}
